a0e0b16fab99efff50da0a5872ae2a027247ed08
jesus <jesusflsa2004@gmail.com>
2024-05-28 01:53:48 -0500

    Add: Libro

---
+++ src/main/java/com/aluracursos/literalura/models/Libro.java
@@ -0,0 +1,77 @@
+package com.aluracursos.literalura.models;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "libros")
+public class Libro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String titulo;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "libros_autores",
+            joinColumns = @JoinColumn(name = "libro_id"),
+            inverseJoinColumns = @JoinColumn(name = "autor_id"))
+    private List<Autor> autores = new ArrayList<>();
+    private String idioma;
+    private Integer numeroDescargas;
+
+    public Libro() {}
+
+    public Libro(DatosLibro datosLibro) {
+        this.titulo = datosLibro.titulo();
+        this.idioma = datosLibro.idiomas().get(0);
+        this.numeroDescargas = datosLibro.numeroDescargas();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public List<Autor> getAutores() {
+        return autores;
+    }
+
+    public void setAutores(List<Autor> autores) {
+        this.autores = autores;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    @Override
+    public String toString() {
+        return """
+                Titulo: %s
+                Autores: %s
+                Idioma: %s
+                Numero de descargas: %s
+                """.formatted(this.titulo,
+                    this.autores.stream().map(Autor::getNombre).toList(),
+                    this.idioma,
+                    this.numeroDescargas);
+    }
+}
